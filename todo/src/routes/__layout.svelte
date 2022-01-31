c22d50c33bcb747e8b16c2d42dd9eff4e251c5e7
j314h <johnhaimez314@gmail.com>
2022-01-31 13:54:45 +0100

    ajout composant et store error

--- todo/src/routes/__layout.svelte
+++ todo/src/routes/__layout.svelte
@@ -1,7 +1,10 @@
 <script>
   import '../app.css';
   import Main from '$lib/modules/main/Main.component.svelte';
+  import Error from '$lib/modules/error/Error.component.svelte';
 </script>
+
+<Error />
 
 <Main>
   <slot />
