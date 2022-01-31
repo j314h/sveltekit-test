ac03e31dda2014a7e2f5ecab361f4f9df6aa6ac4
j314h <johnhaimez314@gmail.com>
2022-01-31 10:59:24 +0100

    install env-cmd + init supabase

---
+++ todo/src/env.d.ts
@@ -0,0 +1,10 @@
+// nos variable du .env
+interface ImportMetaEnv {
+    readonly VITE_URL_SUPABASE: string;
+    readonly VITE_TOKEN_PUBLIC_SUPABASE: string;
+  }
+  
+// ajoute notre interface qui contiendra nos variable dans le meta de import
+interface ImportMeta {
+readonly env: ImportMetaEnv;
+}
