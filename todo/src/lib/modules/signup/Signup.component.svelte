2bbe898b959466ece314d5556975799dd41cdafe
David-Caignaert <davidcaignaert@gmail.com>
2022-01-31 14:09:59 +0100

    formulaire d inscription

---
+++ todo/src/lib/modules/signup/Signup.component.svelte
@@ -0,0 +1,47 @@
+<div class="card card-bordered">
+  <div class="card-body">
+    <h2>Formulaire d'inscription</h2>
+    <form action="">
+      <div class="form-control">
+        <label for="label">
+          <span class="label-text">Nom</span>
+        </label>
+        <input type="text" required={true} placeholder="Nom" class="input input-ghost" />
+      </div>
+      <div class="form-control">
+        <label for="label">
+          <span class="label-text">Prénom</span>
+        </label>
+        <input type="text" required={true} placeholder="Prénom" class="input input-ghost" />
+      </div>
+      <div class="form-control">
+        <label for="label">
+          <span class="label-text">Surnom</span>
+        </label>
+        <input type="text" required={true} placeholder="Surnom" class="input input-ghost" />
+      </div>
+      <div class="form-control">
+        <label for="label">
+          <span class="label-text">Email</span>
+        </label>
+        <input type="text" required={false} placeholder="Email" class="input input-ghost" />
+      </div>
+      <div class="form-control">
+        <label for="label">
+          <span class="label-text">Adresse</span>
+        </label>
+        <input type="text" required={true} placeholder="Adresse" class="input input-ghost" />
+      </div>
+      <div class="form-control">
+        <label for="label">
+          <span class="label-text">Code postale</span>
+        </label>
+        <input type="text" required={true} placeholder="62119" class="input input-ghost" />
+        <label for="label">
+          <span class="label-text">Ville</span>
+        </label>
+        <input type="text" required={true} placeholder="Dourges" class="input input-ghost" />
+      </div>
+    </form>
+  </div>
+</div>
--- todo/src/routes/index.svelte
+++ todo/src/routes/index.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
   import Signin from '$lib/modules/signin/Signin.component.svelte';
+  import Signup from '$lib/modules/signup/Signup.component.svelte';
 </script>
 
 <h1 class="font-bold text-3xl">La todo du futur</h1>
 <!-- accueil -->
 <Signin />
+
+<!-- inscription -->
+<Signup />
