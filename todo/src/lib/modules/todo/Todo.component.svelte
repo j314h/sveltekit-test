dd709004bc34b2a115ff721cec6835cb9e6bc1de
David-Caignaert <davidcaignaert@gmail.com>
2022-02-02 16:40:38 +0100

    ajustement label todo

--- todo/src/lib/modules/todo/Todo.component.svelte
+++ todo/src/lib/modules/todo/Todo.component.svelte
@@ -76,18 +76,12 @@
           await changeCheck(e, todo.id);
         }}
       />
-      <span class="label-text">{todo.text}</span>
-
-      <!-- btn delete -->
-      <button
-        class="text-red-500 ml-4"
-        on:click={async () => {
-          await deleteTodo(todo.id);
-        }}
-      >
+      <span class="label-text ml-4">{todo.text}</span>
+      <div class="flex">
+        <!-- btn change text -->
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5"
+          class="h-6 w-6"
           viewBox="0 0 20 20"
           fill="currentColor"
         >
@@ -95,25 +89,31 @@
             d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
           />
         </svg>
-      </button>
+        <button on:click={updateChange} />
 
-      <!-- btn change text -->
-      <button on:click={updateChange}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+        <!-- btn delete -->
+        <button
+          class="text-red-500 ml-4"
+          on:click={async () => {
+            await deleteTodo(todo.id);
+          }}
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-          />
-        </svg>
-      </button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+            />
+          </svg>
+        </button>
+      </div>
     </label>
   </section>
 {:else if todo && btnUpdate}
