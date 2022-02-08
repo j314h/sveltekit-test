10620e5c24ecfb1f6030e0a1ca67d2137fde891f
j314h <johnhaimez314@gmail.com>
2022-02-08 15:32:36 +0100

    fixes #41 ; ajout de l'oeil sur le input password

--- todo/src/lib/modules/signin/Signin.component.svelte
+++ todo/src/lib/modules/signin/Signin.component.svelte
@@ -4,6 +4,8 @@
   import { createObjectAsFormData } from 'woo-format';
 
   let styleLoader = '';
+  let typePwd = 'password';
+  $: seePassword = typePwd === 'password' ? false : true;
 
   // envoie formulaire connexion user
   const handlerConnexion = async (e) => {
@@ -28,6 +30,14 @@
     e.target.reset();
     styleLoader = '';
   };
+
+  const handlerSeePwd = () => {
+    if (typePwd === 'password') {
+      typePwd = 'text';
+    } else {
+      typePwd = 'password';
+    }
+  };
 </script>
 
 <section class="card shadow-lg card-bordered my-12">
@@ -48,13 +58,57 @@
       </div>
       <!-- input mot de passe -->
       <div class="mt-2 mb-12">
-        <input
-          type="password"
-          placeholder="Mot de passe"
-          class="input input-primary input-bordered"
-          name="password"
-          required
-        />
+        <div class="relative">
+          <input
+            type={typePwd}
+            placeholder="Mot de passe"
+            class="input input-primary input-bordered"
+            name="password"
+            required
+          />
+          <div
+            class="absolute top-3 right-4 hover:text-secondary cursor-pointer"
+            on:click={handlerSeePwd}
+          >
+            {#if seePassword}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                />
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                />
+              </svg>
+            {:else}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21"
+                />
+              </svg>
+            {/if}
+          </div>
+        </div>
         <!-- mot de passe oublié -->
         <div class="text-right">
           <a
