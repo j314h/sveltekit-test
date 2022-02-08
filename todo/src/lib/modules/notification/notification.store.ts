045e1ea5cb7e80baaf5941773bb721f311878cae
David-Caignaert <davidcaignaert@gmail.com>
2022-02-08 10:23:47 +0100

    feat #5; ajout du store fonction ajouter un notification et efface une notification

---
+++ todo/src/lib/modules/notification/notification.store.ts
@@ -0,0 +1,31 @@
+import { writable } from 'svelte/store';
+
+
+const storeNotification = () => {
+  const { set, subscribe, update } = writable([] as string[]); 
+
+  return {
+    set,
+    subscribe,
+
+    // ajouter une nouvelle notification
+    addNewNotification: (value: string): void => {
+      update((n) => {
+        // si la notification existe déjà on la retire
+        n = n.filter((el) => el !== value)
+        n = [...n, value];
+        return n;
+      })
+    },
+
+    // supprimer une notification
+    removeNotification: (value: string): void => {
+      update((n) => {
+        n = n.filter((el) => el !== value)
+        return n;
+      })
+    }
+  }
+}
+
+export const notificationStore = storeNotification();
