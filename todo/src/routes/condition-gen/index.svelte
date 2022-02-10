225d5d768702099ccbb7e5c62491258a1c746292
j314h <johnhaimez314@gmail.com>
2022-02-10 14:06:06 +0100

    ajout article condition gene

--- todo/src/routes/condition-gen/index.svelte
+++ todo/src/routes/condition-gen/index.svelte
@@ -100,5 +100,52 @@
         à la protection des données de woopear à l’adresse woopears@gmail.com
       </p>
     </article>
+
+    <!-- article 4 -->
+    <article class="mb-12">
+      <h2 class="font-bold text-primary mb-6">Liens hypertextes et cookies :</h2>
+      <p class="text-justify md:pl-4 lg:w-8/12">
+        Le site <span class="text-primary block"> https://la-todo-du-future.herokuapp.com/ </span> contient
+        un certain nombre de liens hypertextes vers d'autres sites, mis en place avec l'autorisation
+        de Woopear. Cependant, Woopear n'a pas la possibilité de vérifier le contenu des sites ainsi
+        visités, et n'assumera en conséquence aucune responsabilité de ce fait.
+      </p>
+      <p class="text-justify md:pl-4 lg:w-8/12 mt-6">
+        La navigation sur le site <span class="text-primary block">
+          https://la-todo-du-future.herokuapp.com/
+        </span> est susceptible de provoquer l'installation de cookie(s) ou de données dans le localstorage
+        sur l'ordinateur de l'utilisateur. Un cookie est un fichier de petite taille, qui ne permet pas
+        l'identification de l'utilisateur, mais qui enregistre des informations relatives à la navigation
+        d'un ordinateur sur un site, le localstorage utilise le même procèdé. Les données ainsi obtenues
+        visent à faciliter la navigation ultérieure sur le site, et ont également vocation à permettre
+        diverses mesures de fréquentation.
+      </p>
+      <p class="text-justify md:pl-4 lg:w-8/12 mt-6">
+        Le site <span class="text-primary block"> https://la-todo-du-future.herokuapp.com/ </span> n'utilise
+        aucun cookie, mais utilise le stockage de quelques données dans le localstorage pour le bon fonctionnement
+        du site. Toutes personnes accèptant ces conditions générales accèpte le stoage de données dans
+        le localstorage du navigateur.
+      </p>
+    </article>
+
+    <!-- article 5 -->
+    <article class="mb-12">
+      <h2 class="font-bold text-primary mb-6">Droit applicable et attribution de juridiction :</h2>
+      <p class="text-justify md:pl-4 lg:w-8/12">
+        Tout litige en relation avec l'utilisation du site <span class="text-primary block">
+          https://la-todo-du-future.herokuapp.com/
+        </span> est soumis aux tribunaux francais compétents statuant selon le droit français.
+      </p>
+    </article>
+
+    <!-- article 6 -->
+    <article class="mb-12">
+      <h2 class="font-bold text-primary mb-6">Les principales lois concernées :</h2>
+      <p class="text-justify md:pl-4 lg:w-8/12">
+        Loi n° 78-87 du 6 janvier 1978, notamment modifiée par la loi n° 2004-801 du 6 août 2004
+        relative à l'informatique, aux fichiers et aux libertés. Loi n° 2004-575 du 21 juin 2004
+        pour la confiance dans l'économie numérique
+      </p>
+    </article>
   </section>
 </section>
