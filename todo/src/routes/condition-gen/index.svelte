513206b30a65e03b99c35a81ca1446e7701b0382
j314h <johnhaimez314@gmail.com>
2022-02-10 11:51:18 +0100

    creation de la page condition général

---
+++ todo/src/routes/condition-gen/index.svelte
@@ -0,0 +1,104 @@
+<script lang="ts">
+</script>
+
+<section class="card card-bordered bg-base-100 shadow-lg w-11/12 mb-16">
+  <section class="card-body">
+    <h1 class="card-title text-center text-3xl font-bold mb-24">Conditions général</h1>
+    <!-- article 1 -->
+    <article class="mb-12">
+      <h2 class="font-bold text-primary mb-6">Informations légales :</h2>
+      <p class="text-justify md:pl-4 lg:w-8/12">
+        Le site, accessible à l’adresse
+        <span class="text-primary block"> https://la-todo-du-future.herokuapp.com/ </span> est édité
+        par <a>Woopear</a>, exerçant sous la forme d’une association dont le siège social est situé
+        au 24 impasse en pesse monture 25310 Hérimoncourt, immatriculée sous le numéro W252006839
+      </p>
+      <p class="text-justify md:pl-4 mt-6 lg:w-8/12">
+        Le directeur de la publication est Mr. HAIMEZ John
+      </p>
+      <p class="text-justify md:pl-4 mt-6 lg:w-8/12">
+        Le site est hébergé par la société <a
+          href="https://www.salesforce.com/company/privacy/"
+          target="_blank"
+          class="text-primary">Salesforce</a
+        > Data Protection Officer (Salesforce Privacy Team) (and in India, a Grievance Officer) 415 Mission
+        St., 3rd Floor San Francisco, CA 94105, USA
+      </p>
+      <p class="text-justify md:pl-4 mt-6 lg:w-8/12">
+        Toutes les données sont stocker sur <a
+          href="https://supabase.com/docs/company/privacy#9-contact-us"
+          target="_blank"
+          class="text-primary">Supabase Inc</a
+        >.
+      </p>
+    </article>
+
+    <!-- article 2 -->
+    <article class="mb-12">
+      <h2 class="font-bold text-primary mb-6">Conditions d’utilisation :</h2>
+      <p class="text-justify md:pl-4 lg:w-8/12">
+        Le site accessible par les url suivants : <span class="text-primary block">
+          https://la-todo-du-future.herokuapp.com/
+        </span> est exploité dans le respect de la législation française. L’utilisation de ce site est
+        régie par les présentes conditions générales. En utilisant le site, vous reconnaissez avoir pris
+        connaissance de ces conditions et les avoir acceptées.
+      </p>
+      <p class="text-justify md:pl-4 lg:w-8/12 mt-6">
+        Celles-ci pourront être modifiées à tout moment et sans préavis par l'association – <a
+          >Woopear</a
+        > – HAIMEZ John ne saurait être tenu pour responsable en aucune manière d’une mauvaise utilisation
+        du service.
+      </p>
+    </article>
+
+    <!-- article 3 -->
+    <article class="mb-12">
+      <h2 class="font-bold text-primary mb-6">Protection des données à caractère personnel :</h2>
+      <p class="text-justify md:pl-4 lg:w-8/12">
+        Conformément à la réglementation en vigueur, Woopear met en œuvre un traitement de données à
+        caractère personnel. Dans le cadre du site Internet <span class="text-primary block">
+          https://la-todo-du-future.herokuapp.com/
+        </span>, les personnes concernées sont les internautes visitant ce site Internet et les
+        utilisateurs de ce service.
+      </p>
+      <p class="text-justify md:pl-4 lg:w-8/12 mt-6">
+        Les données à caractère personnel concernées par la présente Politique de Confidentialité
+        sont donc toutes données se rapportant à ces internautes et utilisateurs, dans la mesure où
+        ils sont identifiés ou identifiables, par exemple au moyen de l’adresse IP de leur terminal
+        ou de leur identifiant de compte utilisateur.
+      </p>
+      <p class="text-justify md:pl-4 lg:w-8/12 mt-6">
+        Les traitements de données à caractère personnel sont soumis à un cadre légal et
+        réglementaire spécifique, constitué pour la France essentiellement par le RGPD et la loi
+        dite « Informatique et Libertés » du 6 janvier 1978 telle que dernièrement modifiée par la
+        loi relative à la protection des données personnelles du 20 juin 2018.
+      </p>
+      <p class="text-justify md:pl-4 lg:w-8/12 mt-6">
+        L’Éditeur informe l’utilisateur de la mise en œuvre d’un traitement de données à caractère
+        personnel le concernant, destiné aux membres habilités de Woopear, ainsi qu’à l’Éditeur et
+        ses sous-traitants.
+      </p>
+      <p class="text-justify md:pl-4 lg:w-8/12 mt-6">
+        L’utilisateur dispose d’un droit d’opposition, d’un droit à la limitation, d’un droit
+        d’accès, d’un droit de rectification, d’un droit à la portabilité et d’un droit à
+        l’effacement de ses données.
+      </p>
+      <p class="text-justify md:pl-4 lg:w-8/12 mt-6">
+        L’utilisateur dispose également de la faculté de donner des directives à l’Éditeur sur le
+        sort de ses données après son décès.
+      </p>
+      <p class="text-justify md:pl-4 lg:w-8/12 mt-6">
+        L’Éditeur peut conserver les données à caractère personnel des utilisateurs non clients à
+        des fins de prospection pour une durée de trois ans à compter de leur collecte ou du dernier
+        contact.
+      </p>
+      <p class="text-justify md:pl-4 lg:w-8/12 mt-6">
+        Pour exercer ses droits, l’utilisateur doit adresser un courrier à woopear accompagné de la
+        photocopie d’un titre d’identité comportant sa signature à l’adresse email
+        woopears@gmail.com ou à l’adresse postale suivante : woopear, 24 impasse en pesse monture,
+        25310 Hérimoncourt Pour toute information complémentaire, vous pouvez contacter le référent
+        à la protection des données de woopear à l’adresse woopears@gmail.com
+      </p>
+    </article>
+  </section>
+</section>
